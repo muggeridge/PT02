5230421cb257149698c1eda1b11a15278e6049af
christian <christian.muggeridge@gmail.com>
2024-04-09 14:35:05 -0400

    Remove code which generated last name array for old user select

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,12 +5,5 @@ class ProjectsController < ApplicationController
     end
     def show 
         @project = Project.find(params[:id])
-
-        #make last name array accessible to form for user addition from UI
-        @user_last_names = ["<Select User>"]
-        User.all.each do |user|
-            @user_last_names << user.last_name
-        end
-        @user_last_names.sort!
     end
 end
