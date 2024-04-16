e2f63b38b576ab23687edfa8e813ba35b8c693ee
christian <christian.muggeridge@gmail.com>
2024-04-16 13:59:31 -0400

    Refactor code surrounding project roles modification from project page

--- app/controllers/project_roles_controller.rb
+++ app/controllers/project_roles_controller.rb
@@ -1,12 +1,19 @@
 
 class ProjectRolesController < ApplicationController
     def create
-        project_id = params[:project_role][:project_id]
-        if ProjectRole.create(project_id: project_id, 
-                              user_id: params[:project_role][:user_id])
-            redirect_to :controller => 'projects', :action => 'show', :id => project_id
+        if ProjectRole.create(project_role_params)
+            redirect_to :controller => 'projects', :action => 'show', :id => project_role_params[:project_id]
         else
             redirect_to root_path
         end
+    end
+
+    private
+
+    def project_role_params
+        params.require(:project_role).permit(
+            :user_id, 
+            :project_id, 
+        )
     end
 end
